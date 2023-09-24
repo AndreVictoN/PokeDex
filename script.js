bdc44d8cc25ba2c7c1b5b90da7b8bb84480be1ce
André Victor Gonçalves Nascimento <135927008+AndreVictoN@users.noreply.github.com>
2023-09-24 19:37:57 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,130 @@
+//#region consts 
+const botao = document.querySelector("#botao");
+const botao2 = document.querySelector("#botao2");
+const venusaurs = document.querySelector("#venusaurs");
+const charizards = document.querySelector("#charizards");
+const blastoises = document.querySelector("#blastoises");
+const raichus = document.querySelector("#raichus");
+const nidokings = document.querySelector("#nidokings");
+const golbats = document.querySelector("#golbats");
+const persians = document.querySelector("#persians");
+const primeapes = document.querySelector("#primeapes");
+const arcanines = document.querySelector("#arcanines");
+const poliwraths = document.querySelector("#poliwraths");
+const alakazams = document.querySelector("#alakazams");
+const machamps = document.querySelector("#machamps");
+const victreebels = document.querySelector("#victreebels");
+const golems = document.querySelector("#golems");
+const rapidashs = document.querySelector("#rapidashs");
+const dodrios = document.querySelector("#dodrios");
+const muks = document.querySelector("#muks");
+const gengars = document.querySelector("#gengars");
+const onixs = document.querySelector("#onixs");
+const hypnos = document.querySelector("#hypnos");
+const kinglers = document.querySelector("#kinglers");
+const marowaks = document.querySelector("#marowaks");
+const hitmonlees = document.querySelector("#hitmonlees");
+const hitmonchans = document.querySelector("#hitmonchans");
+const rhydons = document.querySelector("#rhydons");
+const kangaskhans = document.querySelector("#kangaskhans");
+const mrmimes = document.querySelector("#mrmimes");
+const scythers = document.querySelector("#scythers");
+const electabuzzs = document.querySelector("#electabuzzs");
+const magmars = document.querySelector("#magmars");
+const pinsirs = document.querySelector("#pinsirs");
+const tauross = document.querySelector("#tauross");
+const gyaradoss = document.querySelector("#gyaradoss");
+const laprass = document.querySelector("#laprass");
+const dittos = document.querySelector("#dittos");
+const eevees = document.querySelector("#eevees");
+const kabutopss = document.querySelector("#kabutopss");
+const aerodactyls = document.querySelector("#aerodactyls");
+const snorlaxs = document.querySelector("#snorlaxs");
+const articunos = document.querySelector("#articunos");
+const zapdoss = document.querySelector("#zapdoss");
+const moltress = document.querySelector("#moltress");
+const dragonites = document.querySelector("#dragonites");
+const mewtwos = document.querySelector("#mewtwos");
+const mews = document.querySelector("#mews");
+//#endregion
+
+//#region troca
+botao.addEventListener("click", function() {
+    venusaurs.setAttribute("src", "venusaurs.png");
+    charizards.setAttribute("src", "charizards.png");
+    blastoises.setAttribute("src", "blastoises.png");
+    raichus.setAttribute("src", "raichus.png");
+    nidokings.setAttribute("src", "nidokings.png");
+    golbats.setAttribute("src", "golbats.png");
+    persians.setAttribute("src", "persians.png");
+    primeapes.setAttribute("src", "primeapes.png");
+    arcanines.setAttribute("src", "arcanines.png");
+    poliwraths.setAttribute("src", "poliwraths.png");
+    alakazams.setAttribute("src", "alakazams.png");
+    machamps.setAttribute("src", "machamps.png");
+    victreebels.setAttribute("src", "victreebels.png");
+    golems.setAttribute("src", "golems.png");
+    rapidashs.setAttribute("src", "rapidashs.png");
+    dodrios.setAttribute("src", "dodrios.png");
+    muks.setAttribute("src", "muks.png");
+    gengars.setAttribute("src", "gengars.png");
+    onixs.setAttribute("src", "onixs.png");
+    hypnos.setAttribute("src", "hypnos.png");
+    kinglers.setAttribute("src", "kinglers.png");
+    marowaks.setAttribute("src", "marowaks.png");
+    hitmonlees.setAttribute("src", "hitmonlees.png");
+    hitmonchans.setAttribute("src", "hitmonchans.png");
+    rhydons.setAttribute("src", "rhydons.png");
+    kangaskhans.setAttribute("src", "kangaskhans.png");
+    mrmimes.setAttribute("src", "mrmimes.png");
+    scythers.setAttribute("src", "scythers.png");
+    electabuzzs.setAttribute("src", "electabuzzs.png");
+    magmars.setAttribute("src", "magmars.png");
+    pinsirs.setAttribute("src", "pinsirs.png");
+    tauross.setAttribute("src", "tauross.png");
+    gyaradoss.setAttribute("src", "gyaradoss.png");
+    laprass.setAttribute("src", "laprass.png");
+    dittos.setAttribute("src", "dittos.png");
+    eevees.setAttribute("src", "eevees.png");
+    kabutopss.setAttribute("src", "kabutopss.png");
+    aerodactyls.setAttribute("src", "aerodactyls.png");
+    snorlaxs.setAttribute("src", "snorlaxs.png");
+    articunos.setAttribute("src", "articunos.png");
+    zapdoss.setAttribute("src", "zapdoss.png");
+    moltress.setAttribute("src", "moltress.png");
+    dragonites.setAttribute("src", "dragonites.png");
+    mewtwos.setAttribute("src", "mewtwos.png");
+    mews.setAttribute("src", "mews.png");
+});
+//#endregion
+
+//#region mega
+botao2.addEventListener("click", function() {
+    venusaurs.setAttribute("src", "venusaurm.gif");
+    charizards.setAttribute("src", "charizardm.gif");
+    blastoises.setAttribute("src", "blastoisem.gif");
+    alakazams.setAttribute("src", "alakazamm.gif");
+    gengars.setAttribute("src", "gengarm.gif");
+    kangaskhans.setAttribute("src", "kangaskhanm.gif");
+    gyaradoss.setAttribute("src", "gyaradosm.gif");
+    mewtwos.setAttribute("src", "mewtwom.gif");
+    scythers.setAttribute("src", "scizorm.gif");
+});
+//#endregion
+
+//#region 
+botao3.addEventListener("click", function() {
+    venusaurs.setAttribute("src", "venusaurg.gif");
+    charizards.setAttribute("src", "charizardg.gif");
+    blastoises.setAttribute("src", "blastoiseg.gif");
+    raichus.setAttribute("src", "pikachum.gif");
+    persians.setAttribute("src", "persiang.gif");
+    machamps.setAttribute("src", "machampg.gif");
+    gengars.setAttribute("src", "gengarg.gif");
+    kinglers.setAttribute("src", "kinglerg.gif");
+    laprass.setAttribute("src", "laprasg.gif");
+    eevees.setAttribute("src", "eeveeg.gif");
+    snorlaxs.setAttribute("src", "snorlaxg.gif");
+
+});
+//#endregion
